5921645f577369aad08f6c5b6aed7bdceefa9e79
tsvetanvashkov <tsvetan.vashkov@gmail.com>
2025-03-23 22:14:13 +0200

    Add skeleton

---
+++ src/test/java/com/company/notification_system/NotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.notification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
